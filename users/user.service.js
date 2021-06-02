5e21c884c31624447c5912a567013f6cabc344af
daullath <daullathbasha@gmail.com>
2021-06-02 11:17:12 +0530

    nodejs backend code

---
+++ users/user.service.js
@@ -0,0 +1,55 @@
+﻿const config = require('config.json');
+const jwt = require('jsonwebtoken');
+
+// users hardcoded for simplicity
+const users = [{
+    id: 1, username: 'test', password: 'test', firstName: 'Daullath', lastName: 'Shiek',
+    userInfo: {
+        lastLogin: '02 June 2021',
+        acctBalance: '3000',
+        acctData: [{ date: '31-May-2021', desc: 'Room Rent', amount: '700' },
+        { date: '01-Jun-2021', desc: 'Groceries', amount: '20' },]
+    }
+}];
+
+module.exports = {
+    authenticate,
+    getAll,
+    retrieveUserDetails
+};
+
+async function authenticate({ username, password }) {
+    const user = users.find(u => u.username === username && u.password === password);
+
+    if (!user) throw 'Username or password is incorrect';
+
+    // create a jwt token that is valid for 7 days
+    const token = jwt.sign({ sub: user.id }, config.secret, { expiresIn: '7d' });
+
+    return {
+        ...omitPasswordUserInfo(user),
+        token
+    };
+}
+
+async function getAll() {
+    return users.map(u => omitPassword(u));
+}
+
+async function retrieveUserDetails() {
+    const user1 = users[0];
+    console.log(user1);
+    return { ...omitPassword(user1) };
+}
+
+// helper functions
+
+function omitPassword(user) {
+    const { password, ...userWithoutPassword } = user;
+    return userWithoutPassword;
+}
+
+function omitPasswordUserInfo(user) {
+    const { password, userInfo, ...userWithoutPassword } = user;
+    return userWithoutPassword;
+}
---
+++ users/users.controller.js
@@ -0,0 +1,28 @@
+﻿const express = require('express');
+const router = express.Router();
+const userService = require('./user.service');
+
+// routes
+router.post('/authenticate', authenticate);
+router.get('/', getAll);
+router.get('/retrieveUserDetails', retrieveUserDetails);
+
+module.exports = router;
+
+function authenticate(req, res, next) {
+    userService.authenticate(req.body)
+        .then(user => res.json(user))
+        .catch(next);
+}
+
+function getAll(req, res, next) {
+    userService.getAll()
+        .then(users => res.json(users))
+        .catch(next);
+}
+
+function retrieveUserDetails(req, res, next) {
+    userService.retrieveUserDetails()
+        .then(user => res.json(user))
+        .catch(next);
+}
